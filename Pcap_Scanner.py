746004012083e4fd22fa969e9a99b5a0b3fb8441
muhammed-ezzat <mo.ez.e77@gmail.com>
2019-02-18 15:07:49 +0200

    First commit

---
+++ Pcap_Scanner.py
@@ -0,0 +1,98 @@
+from scapy.all import *
+import socket   
+
+
+def host_ports(pack):
+   ips={}
+   info_port=()
+   for pkt in pack:
+      if pkt.getlayer(IP):
+         ip=pkt[IP].src
+         if pkt.getlayer(UDP):
+            port=pkt.sport
+            ips.setdefault(ip, {})
+            if ip in ips:
+               if port in ips[ip]:
+                  pass
+               else:
+                  try:
+                     info_port=('UDP',socket.getservbyport(port))
+                  except:
+                     info_port=('UDP','not defined')
+                  ips.setdefault(ip, {})[port] =info_port
+         elif pkt.getlayer(TCP):
+            port=pkt.sport
+            ips.setdefault(ip, {})
+            if ip in ips:
+               if port in ips[ip]:
+                  pass
+               else:
+                  try:
+                     info_port=('TCP',socket.getservbyport(port))
+                  except:
+                     info_port=('TCP','not defined')
+                  ips.setdefault(ip, {})[port] =info_port
+         elif pkt.getlayer(ICMP):
+           
+            info_port=('ICMP')
+            ips.setdefault(ip, {})[''] =info_port         
+
+   list=sorted(ips.items(), key=lambda x:x[1], reverse = True)
+   return list
+def dis_os(pack):
+   ttl =0
+   win=0
+   os={}
+   sip=0
+   ch=''
+   for pkt in pack:   
+      if pkt.getlayer(IP) and pkt.getlayer(TCP):
+         sip=pkt[IP].src
+         ttl=pkt[IP].ttl
+         win=pkt.window
+         ip=pkt[IP].src
+         if ttl==128:
+            if win==65535 or win ==16384:
+               ch=("Windows:NT kernel 5.x") 
+            elif win==8192:
+               ch= ("Windows:NT kernel 6.x")
+            else:
+               ch= ("Windows:NT kernel")
+         elif ttl==64:
+            if win==5840:
+               ch= ("Linux kernel 2.x ")
+            elif win==5720:
+               ch= ("Google Linux (Android/chromeOS)")
+            elif win==65535:
+               ch= (" FreeBSD")
+            elif win==16384:
+               ch= ("OpenBSD")
+            elif win==32850:
+               ch= ("Solaris")
+            else:
+               ch=(" Unknown")
+         elif ttl==255:
+            if win ==4128:
+               ch= ("iOS 12.4 (Cisco Routers)")
+         else:
+            ch= ("Unknown")
+         if sip not in os:
+            os.setdefault(sip,ch)
+   return os 
+
+def main():
+   pack = rdpcap("/home/muhammed/share/test.pcapng")
+    
+   list=host_ports(pack)
+   os=dis_os(pack)
+   for host,port1 in list:
+      print("[+]    Host: "+host+"    [+]")
+      print("[+]    Open ports and services :"),
+      print (port1),
+      print ("    [+]")
+      print("[+]    Possible OS = "),
+      print (os.get(host)),
+      print("    [+]\n")
+
+if __name__ == '__main__':
+   main()
